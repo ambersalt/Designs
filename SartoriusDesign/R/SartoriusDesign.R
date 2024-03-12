2ef2ac7589d5824bf686c11ed350a91b32aadd37
ambersalt <163148648+ambersalt@users.noreply.github.com>
2024-03-12 15:50:18 +0100

    Update SartoriusDesign.R

--- SartoriusDesign/R/SartoriusDesign.R
+++ SartoriusDesign/R/SartoriusDesign.R
@@ -1,10 +1,3 @@
-library(usethis)
-library(devtools)
-library(roxygen2)
-
-
-use_vignette(name = "SartoriusDesign",
-             title = "Sartoris Design")
 
 SartoriusDesign <- function(n = 3) {
   colors <- c("#FFED00", "#FFFFFF", "#000000")
